069c0096f501b04c2d8135f3276bd592cb16f30c
Matieshan <mats@ingsys.net>
2019-06-03 10:32:43 +0800

    修改了usermapper

--- store/src/main/java/cn/tedu/store/mapper/UserMapper.java
+++ store/src/main/java/cn/tedu/store/mapper/UserMapper.java
@@ -14,12 +14,7 @@ public interface UserMapper {
 	 */
 	Integer addnew(User user);
 	
-	/**
-	 * 根据用户名查询用户数据
-	 * @param username 用户名
-	 * @return 匹配的用户数据，如果没有匹配的数据，则返回null
-	 */
-	User findByUsername(String username);
+	
 	
 }
 
